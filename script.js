ebdd4d9fe68b76827ea88181241bf71d60d8c0ff
preciousfeature <143934082+preciousfeature@users.noreply.github.com>
2025-06-23 12:25:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ document.getElementById("quizForm").addEventListener("submit", function(e) {
   e.preventDefault();
   let score = 0;
 
-  // Example answer check
   const answers = {
-    q1: "b",
-    // add others like q2: "a", q3: "c"
+    q1: "b", // Black Sabbath
+    q2: "b", // John
+    q3: "b", // The Osbournes
+    q4: "c", // Bit the head off a bat
+    q5: "a", // Blizzard of Ozz
+    q6: "b", // Urinated on monument
+    q7: "b"  // Randy Rhoads
   };
 
   for (let key in answers) {
